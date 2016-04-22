fc5af88f4a607c764fbf336501c98cff7b26adf1
liu_chonghui <liuchonghui@139.com>
2016-04-22 18:32:52 +0800

    commit changes

--- ImageLoaderCompact/samples/app/src/main/java/tool/imageloadercompact/activity/BaseFragmentActivity.java
+++ ImageLoaderCompact/samples/app/src/main/java/tool/imageloadercompact/activity/BaseFragmentActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 
+import com.android.imageloadercompact.ImageLoaderCompact;
+
 //import com.sina.fresco.FrescoManager;
 
 /**
@@ -18,9 +20,9 @@ public class BaseFragmentActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (!FrescoManager.getInstance().isInitialized()) {
-//            FrescoManager.getInstance().onStart();
-//        }
+        if (!ImageLoaderCompact.getInstance().isInitialized()) {
+            ImageLoaderCompact.getInstance().onStart();
+        }
     }
 
     @Override
