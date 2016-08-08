b1ac736729953088cf626edb66f9081bde9bfaff
liuchonghui <liuchonghui@xiaomi.com>
2016-08-08 16:24:15 +0800

    update version of WindowAttacher to 1.0.0
    N/A
    
    Signed-off-by: liuchonghui <liuchonghui@xiaomi.com>

---
+++ ImageLoaderCompact/WindowAttacher/WindowAttacher/src/com/sina/sinagame/windowattacher/CenterWindowAttacher.java
@@ -0,0 +1,31 @@
+package com.sina.sinagame.windowattacher;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+/**
+ * Created by liuchonghui on 16/8/8.
+ */
+@SuppressLint("ValidFragment")
+public class CenterWindowAttacher extends FooterWindowAttacher {
+
+    public CenterWindowAttacher(Activity attachedActivity,
+                                Fragment attachedFragment, int layoutResId) {
+        super(attachedActivity, attachedFragment, layoutResId);
+    }
+
+    public CenterWindowAttacher(Activity attachedActivity,
+                                Fragment attachedFragment, int layoutResId, int animationViewGroupId) {
+        super(attachedActivity, attachedFragment, layoutResId,
+                animationViewGroupId);
+    }
+
+    public CenterWindowAttacher(Activity attachedActivity, int layoutResId) {
+        super(attachedActivity, layoutResId);
+    }
+
+    public CenterWindowAttacher(Activity attachedActivity, int layoutResId,
+                                int animationViewGroupId) {
+        super(attachedActivity, layoutResId, animationViewGroupId);
+    }
+}
